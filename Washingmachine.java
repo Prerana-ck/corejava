bb52c0385449ef9d40ea3590f500922448d48db0
Prerana-ck <preranack.xworkz@gmail.com>
2024-07-03 05:01:52 +0530

    Basic java code

---
+++ Washingmachine.java
@@ -0,0 +1,16 @@
+class Washingmachine{
+
+  public static void main(String[] lg){
+	  
+	  //display message
+
+   System.out.println("Fully-automatic front load washing machine with Hygiene Steam/direct-drive technology: Best Wash Quality, Energy and Water efficient");
+   System.out.println("Capacity 8 kg: Suitable for large families");
+   System.out.println("1400 RPM: higher spin speeds helps in faster drying");
+   System.out.println("Manufacturer Warranty: 2 Years Comprehensive & 10 Years on Motor T&C");
+   System.out.println("Energy Star rating: 5 Star best in class efficiency; Energy consumption –0.062 KWh/kg/cycle & Water Consumption: 8.250 L/Kg/Cycle");	  
+	  
+  }
+  
+   
+   }
